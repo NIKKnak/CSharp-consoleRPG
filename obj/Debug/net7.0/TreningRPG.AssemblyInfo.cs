d6798543238ce94c35a6a735aafc9371ee378483
NIKKnak <nik88350759@yandex.ru>
2024-01-16 17:45:03 +0300

    11lok

--- obj/Debug/net7.0/TreningRPG.AssemblyInfo.cs
+++ obj/Debug/net7.0/TreningRPG.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreningRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff93a8cb47a6f96c124b0b766f7942452e6f940")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e5e66b5004de97d30dce85f48726f25d9e83f5")]
->>>>>>> 0ff93a8cb47a6f96c124b0b766f7942452e6f940
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ebda796d0d0abf0832c5acc324f72928b2fbfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreningRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreningRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
